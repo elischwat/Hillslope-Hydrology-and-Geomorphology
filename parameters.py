c55337dec06bc8c7471f6a88720050884e36f39b
Eli Schwat <eli.l.schwat@gmail.com>
2018-04-19 01:51:58 -0500

    added comments

--- parameters.py
+++ parameters.py
@@ -10,29 +10,28 @@ import matplotlib.pyplot as plt
 plt.rcParams["font.family"] = "Times New Roman"
 
 
-#hilltype = 'con'
 curvature = 'convex'
+
 #SPACE:
-dx = 1.0 # rec: 10 > dx > 0.0001
-L = 100.0
+dx = 1.0 # Space step for numerical simulation of hillslope evolution.
+L = 100.0 # Horizontal extent of hillslope
 xRes = int(L/dx)
 #TIME:
-dtGeo = 10.0 # years rec: 100 > dt > 1
-T = 10000
+dtGeo = 10.0 # Time step for numerical simulation of hillslope evolution.
+T = 10000 # Length of hillslope evolution simulation.
 tResGeo = int(T/dtGeo)
 
 
 # GEOMORPH STUFF:
-R_dis = 2* 10**-9
-#R_dis = 0
-D = 0.01 # m^2/yr,  from Fernandes 1997 for humid temperate climates
-bmid = 20
-c = 0.2
+D = 0.01 # m^2/yr,  Soil creep diffusivity
+bmid = 20 # m, Countour width at x = L/2
+c = 0.2 # m/m, Magnitude of convergence/divergence rate 
 
 # GEOCHEMISTRY STUFF
-soilDensity = 1.25 * 10**3 #kg/m^3
-dz = 0.1
-ralb = 0.5
+R_dis = 2* 10**-9 # mol/m^2/s Mineral dissolution rate for chem denudation estimates.
+soilDensity = 1.25 * 10**3 # kg/m^3, Density of soil.
+dz = 0.1 # m, Thickness of saprolite layer in which mineral dissolution is accounted for.
+ralb = 0.5 # Percent of saprolite composed of mineral for which dissolution is accounted for.
 Ds = .001
 #As = 4*np.pi*(0.5*Ds)**2 #spherical 
 As = 6*Ds**2 #cubic
